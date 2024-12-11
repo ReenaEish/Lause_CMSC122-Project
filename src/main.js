f61d1eb307f20bf7130c88ebb75a07664946d029
ReenaEish <gadgetmaster90@gmail.com>
2024-12-11 15:26:14 +0800

    Buttons now working, Proceeding to Main board

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
+    // Main (Includes Restart Button and Back Buttons)
+    
     // DOM Elements
     const mainMenu = document.getElementById('main-menu');
     const gameBoard = document.getElementById('game-board');
@@ -7,14 +9,14 @@ document.addEventListener('DOMContentLoaded', () => {
     // Buttons
     const newGameBtn = document.getElementById('new-game-btn');
     const howToPlayBtn = document.getElementById('how-to-play-btn');
-    const backToMenuGameBtn = document.getElementById('back-to-menu-game-btn');
-    const backToMenuHowBtn = document.getElementById('back-to-menu-how-btn');
+    const backToMenuGameBtn = document.getElementById('back-to-menu-btn');
+    const backToMenuBtnHow = document.getElementById('back-to-menu-btn-how');
 
     // Event Listeners
     newGameBtn.addEventListener('click', () => toggleSection(gameBoard));
     howToPlayBtn.addEventListener('click', () => toggleSection(howToPlay));
     backToMenuGameBtn.addEventListener('click', () => toggleSection(mainMenu));
-    backToMenuHowBtn.addEventListener('click', () => toggleSection(mainMenu));
+    backToMenuBtnHow.addEventListener('click', () => toggleSection(mainMenu));
 
     // Function to Toggle Sections
     function toggleSection(section) {
---
+++ src/solver.js
@@ -0,0 +1,1 @@
+// Give up Button - Auto Solver
